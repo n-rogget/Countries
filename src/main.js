8d9b73be866326e313ed46d2386b7d7e084fff0d
ginapedraza <ginapedraza00@gmail.com>
2023-07-25 18:13:55 -0400

    accediendo a los datos de banderas y nombres

--- src/main.js
+++ src/main.js
@@ -5,5 +5,22 @@ import data from './data/countries/countries.js';
 
 console.log(data);
 
-const primerPais = data["countries"][0];
-console.log(primerPais);
+//const primerPais = data[][0];
+//console.log(primerPais["name"]["common"]);
+
+const nombresPaises = [];
+for (let i = 0; i < data.countries.length; i++) {
+    const pais = data.countries[i];
+    nombresPaises.push(pais.name.common);
+}
+
+console.log(nombresPaises);
+
+const banderasPaises = [];
+for (let i = 0; i < data.countries.length; i++) {
+    const bandera = data.countries[i];
+    banderasPaises.push(bandera.flags.png);
+
+}
+
+console.log(banderasPaises);
