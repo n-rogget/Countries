d588af414d2b7919056bcec00b9260224779d8ac
ginapedraza <ginapedraza00@gmail.com>
2023-07-26 18:40:39 -0400

    Redactando la función para mostrar las cards pero con errores

--- src/main.js
+++ src/main.js
@@ -25,20 +25,63 @@ for (let i = 0; i < data.countries.length; i++) {
 
 console.log(banderasPaises);*/
 
-const containerCard = document.querySelector(".grid-container")  //Para que aparezca en la sección
-const createCards = (paises) => {                    //creamos las tarjetas con parametro países, para poder ocupar este parametro más adelante  
-    for (let i = 0; i < paises.length; i++) {         //Recorremos los paises       
+
+
+//VISUALIZAR LOS PAISES EN LA PANTALLA
+const containerCard = document.querySelector(".grid-container") //Para que aparezca en la sección
+ 
+ const createCards = (paises) => {                    //creamos las tarjetas con parametro países, para poder ocupar este parametro más adelante  
+    for (let i = 0; i < paises.length; i++) {     //Recorremos los paises
+         
         const card = document.createElement('section')  //creamos una sección     
         card.classList.add('box')                       // añadimos clase a la sección (box porque ya la definimos en css anteriormente)    
         card.innerHTML = `<img src="${paises[i].flags.png}"/>    
-                        <h2>${paises[i].name.common}</h2>`    
-        containerCard.appendChild(card)                 //mandamos a html la imagen y el nombre, con $para que busque lo que le pedimos                                                    
-        // luego appendchile nos trae un "hijo", sacado de un padre (ponemos en grid container la seccion)  
+                        <h2>${paises[i].name.common}</h2>`    //mandamos a html la imagen y el nombre, con $para que busque lo que le pedimos 
+        containerCard.appendChild(card) // luego appendchile nos trae un "hijo", sacado de un padre (ponemos en grid container la seccion)
+          
     }
 }
-createCards(data.countries); //llamamos a la funcion por el arreglo donde debe buscar// 
+createCards(data.countries); 
+
+//MOSTRAR DATA DE CADA PAÍS AL HACER CLICK
+
+
+const showCards = (paises) => {
+    const boxCountries = document.querySelectorAll('.box');
+    for (let i = 0; i < boxCountries.length; i++) {
+        
+        boxCountries[i].addEventListener('click', () => {
+            console.log('Clickeaste la tarjeta:', i);
+            const popUp = document.querySelector('#popupContainer');
+        
+            const popUpWindow = document.createElement('div')
+            popUpWindow.classList.add('popup')
+            popUpWindow.innerHTML = `<img src="${paises[i].flags.png}"/>    
+            <h2>${paises[i].name.common}</h2>
+            <p>Nombre oficial: ${paises[i].name.official}
+            <button onclick= "closePopUp()">Cerrar</button>`; 
+
+            popUp.appendChild(popUpWindow);
+        });
+        
+    };
+};
+
+function closePopUp () {
+    const popUpWindow = document.querySelector('.popup');
+    popUpWindow.remove();
+}
+
+showCards(data.countries);
+
+
+
+
+
+//llamamos a la funcion por el arreglo donde debe buscar// 
 /*const filterByInitialCharacter = (paises, letra) => {  //   
     const paisesPorLetra = []   
     for (let i = 0; i < paises.length; i++) {     
         if (paises[i].name.common[0] === letra.toLowerCase() || paises[i].name.common[0] === letra.toUpperCase()) {       
             paisesPorLetra.push(paises[i])//     }//   } // }// filterByInitialCharacter(data.countries,"C")      // TODO ESTO NOS SIRVE PARA FILTRAR*/
+
