61136a0ff4affc83c4f5148da8420983fd346d07
ginapedraza <ginapedraza00@gmail.com>
2023-07-27 18:17:16 -0400

    Ventana de cada tarjeta abre ok, botón de cerrar funciona, podemos visualizar bien todos los datos menos Borders y Fifa

--- src/main.js
+++ src/main.js
@@ -3,29 +3,6 @@
 import data from './data/countries/countries.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(data);
-
-//const primerPais = data[][0];
-//console.log(primerPais["name"]["common"]);
-
-/*const nombresPaises = [];
-for (let i = 0; i < data.countries.length; i++) {
-    const pais = data.countries[i];
-    nombresPaises.push(pais.name.common);
-}
-
-console.log(nombresPaises);
-
-const banderasPaises = [];
-for (let i = 0; i < data.countries.length; i++) {
-    const bandera = data.countries[i];
-    banderasPaises.push(bandera.flags.png);
-
-}
-
-console.log(banderasPaises);*/
-
-
 
 //VISUALIZAR LOS PAISES EN LA PANTALLA
 
@@ -58,56 +35,95 @@ createCards(data.countries);
 
 //MOSTRAR DATA DE CADA PAÍS AL HACER CLICK
 
+//Creamos una función para obtener un arreglo del objeto Languages
+const getLanguage = (languages) => {
+    const arrayLanguage = Object.values(languages)
+    let languageString = ""
+    for (const language of arrayLanguage){
+     
+      
+      languageString = languageString + language + " "
+      
+    }
+    return languageString;
+  }
+
+  //Creamos una función para obtener el valor de Gini
+  const getGini = (gini) => {
+    if (!gini) {
+        return "No" 
+    }
+    const giniValue = Object.values(gini)
+        return giniValue[0];
+  }
+
+  //Creamos una función para obtener el valor de Capital
+  const getCapital = (capital) => {
+    if (!capital) {
+        return "No" 
+    }
+    const capitalValue = Object.values(capital)
+        return capitalValue;
+  }
+
 
 const showCards = (paises) => {
     // Seleccionamos los elementos de la clase .box
     const boxCountries = document.querySelectorAll('.box');
     //Seleccionamos el elemento section con su ID        
     const popUp = document.querySelector('#popupDialog');
+
+    
     //Recorrremos los elementos de las tarjetas
     for (let i = 0; i < boxCountries.length; i++) {
     //Escuchamos el evento click para cada tarjeta
         boxCountries[i].addEventListener('click', () => {
-  
-    // Creamos un elemento div para mostrar la info
-            //const popUpWindow = document.createElement('div')
-    //Le agregamos la clase popup
-            //popUpWindow.classList.add('popup')
-    //Lo mostramos con el innerHTML y agregamos la data a mostrar
-           // popUpWindow.innerHTML = 
-    
-            popUp.innerHTML =
-            `<img src="${paises[i].flags.png}"/>    
+            console.log(boxCountries[i]);
+    //Guardamos en una constante la data que vamos a mostrar
+           
+            const popContent = `<img src="${paises[i].flags.png}"/>    
             <h2>${paises[i].name.common}</h2>
             <p>Official name: ${paises[i].name.official}</p>
-            <p>TLD: ${paises[i].tld.join(', ')}</p>
+            <p>TLD: ${paises[i].tld}</p>
             <p>Independent: ${paises[i].independent ? 'Yes' : 'No'}</p>
-            <p>Capital: ${paises[i].capital[0]}</p>
+            <p>Capital: ${getCapital(paises[i].capital)}</p>
             <p>Subregion: ${paises[i].subregion}</p>
-            <p>Languages: ${paises[i].languages.spa}</p>
-            <p>Borders: ${paises[i].borders.join(', ')}</p>
+            <p>Languages: ${getLanguage(paises[i].languages)}</p>
+            <p>Borders: ${paises[i].borders}</p>
             <p>Area: ${paises[i].area}</p>
             <p>Flag: ${paises[i].flag}</p>
             <p>Population: ${paises[i].population}</p>
-            <p>Gini: ${paises[i].gini['2014']}</p>
+            <p>Gini: ${getGini(paises[i].gini)}</p>
             <p>Fifa: ${paises[i].fifa}</p>
             <p>Timezones: ${paises[i].timezones[0]}</p>
-            <p>Continents: ${paises[i].continents[0]}</p>
-            <button onclick= "closePopUp()">Cerrar</button>`; 
-
+            <p>Continents: ${paises[i].continents[0]}</p>`;
+    
+    // La vamos a mostrar con innerHTML        
+            popUp.innerHTML = popContent;
+    //Creamos un botón
+            const button = document.createElement("button");
+    //Le agregamos una clase al buttton
+            button.classList.add("closeButton");
+    
+            button.innerHTML = "Cerrar";
+    //Del dialog sale un hijo button
+            popUp.appendChild(button);
+    //Le escuchamos el click al button y cerramos
+            button.addEventListener('click', () => {
+                popUp.close();
+            });
+    //Usamos el método showModal() para mostrar el elemento dialog       
             popUp.showModal();
-        });
-        
-    };
+        });   
+    }
 
-};
+ 
 
-const closePopUp = () => {
-    const popUp = document.querySelector('#popupDialog');
-    popUp.close();
 };
 
 showCards(data.countries);
+
+
 
 
 
